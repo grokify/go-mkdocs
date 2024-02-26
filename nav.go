c59d5f8d4c6ac1c0b3086a396592757607296f2f
grokify <johncwang@gmail.com>
2024-02-26 00:59:01 -0800

    doc: add comments

--- nav.go
+++ nav.go
@@ -4,6 +4,9 @@ import "strings"
 
 type Texts []Text
 
+// Markdown generates a slice of markdown lines. `Texts` is intended to
+// ease the creation of nested key value pairs where they keys may be
+// dynamic values.
 func (t Texts) Markdown(prefix, indent string) []string {
 	var lines []string
 	for _, ti := range t {
