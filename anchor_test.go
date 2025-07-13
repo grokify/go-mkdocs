bf7951b13b102df07b7c05020f63315f5e35d0b0
John Wang <johncwang@gmail.com>
2025-07-13 07:08:35 -0700

    test: `TestStringToAnchor()`: example test case coverage

--- anchor_test.go
+++ anchor_test.go
@@ -4,32 +4,35 @@ import (
 	"testing"
 )
 
-var sliceMinMaxTests = []struct {
-	v    string
-	want string
-}{
-	{
-		"by Jesús",
-		"by-jesus"},
-	{
-		"by João",
-		"by-joao"},
-	{
-		"registered® mark",
-		"registered-mark"},
-	{
-		"pretext (params) suftext",
-		"pretext-params-suftext"},
-	{
-		"trademark™ (mark) from example.com",
-		"trademarktm-mark-from-examplecom"},
-}
-
 func TestStringToAnchor(t *testing.T) {
-	for _, tt := range sliceMinMaxTests {
+	var stringToAnchorTests = []struct {
+		v    string
+		want string
+	}{
+		{
+			"by Jesús",
+			"by-jesus"},
+		{
+			"  by  João  ",
+			"by-joao"},
+		{
+			"registered® mark",
+			"registered-mark"},
+		{
+			"pretext (params) suftext",
+			"pretext-params-suftext"},
+		{
+			"pretext (   params   )    suftext",
+			"pretext-params-suftext"},
+		{
+			"trademark™ (mark) from example.com",
+			"trademarktm-mark-from-examplecom"},
+	}
+
+	for _, tt := range stringToAnchorTests {
 		try := StringToAnchor(tt.v)
 		if try != tt.want {
-			t.Errorf("timeutil.StringToAnchor(\"%s\") Mismatch: want [%s], got [%s]", tt.v, tt.want, try)
+			t.Errorf("mkdocs.StringToAnchor(\"%s\") Mismatch: want [%s], got [%s]", tt.v, tt.want, try)
 		}
 	}
 }
